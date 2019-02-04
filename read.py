637fc5345c51c2db18fa1fef5c1c9b4aa5904fc6
long siang <w2955307@gmail.com>
2019-02-04 12:08:40 +0800

    add filter

--- read.py
+++ read.py
@@ -12,3 +12,9 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('留言平均的長度', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
